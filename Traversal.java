8ec6ed6cce868fcba8ad726fa20acb8e444e0af9
igalna <ijohns03@CSPC115.dcs.bbk.ac.uk>
2014-10-01 14:42:43 +0100

    First Edit

--- Traversal.java
+++ Traversal.java
@@ -1,5 +1,7 @@
 public class Traversal {
 	public static void main(String[] args) {
 		System.out.println("Traversal program");
+		// Comments underneath the print statement
+		System.out.println("This is another line");
 	}
 }
